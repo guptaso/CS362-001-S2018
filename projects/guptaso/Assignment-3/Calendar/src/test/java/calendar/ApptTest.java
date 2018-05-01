31c8548436b2bed6d97cbbf66c303726df1a130f
Sonica Gupta <guptaso@flip3.engr.oregonstate.edu>
2018-05-01 11:23:34 -0700

    added test files

--- projects/guptaso/Assignment-3/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/guptaso/Assignment-3/Calendar/src/test/java/calendar/ApptTest.java
@@ -10,18 +10,82 @@ public class ApptTest  {
 
   @Test(timeout = 4000)
   public void test00()  throws Throwable  {
-      Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+      Appt appt0 = new Appt(15, 30, 9, 14, 2016, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
       String string0 = appt0.toString();
       assertEquals(2, appt0.getRecurBy());
       assertFalse(appt0.isRecurring());
-      assertEquals("\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
+      //assertEquals(null, appt0.getRecurDays());
+      assertEquals("\t14/9/2016 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
       assertEquals(0, appt0.getRecurIncrement());
       appt0.setValid();
+      assertTrue(appt0.hasTimeSet());
+      assertEquals("xyz@gmail.com", appt0.getEmailAddress());
+      
   }
 @Test(timeout = 4000)
  public void test01()  throws Throwable  {
-
-
+	Appt appt1 = new Appt(20, 4, 2018, null, null, null);
+	assertTrue(appt1.isOn(20, 4, 2018));
+	assertFalse(appt1.hasTimeSet());
 }
+
+@Test(timeout = 4000) 
+	public void test02() throws Throwable {
+		Appt appt2 = new Appt(0, 30, 9, 24, 2018, "Nothing", "Empty Day", "person@gmail.com");
+		String string2 = appt2.toString();
+		assertEquals("\t24/9/2018 at 12:30am ,Nothing, Empty Day\n", string2);	
+		appt2.getXmlElement();
+}
+@Test(timeout = 4000) 
+	public void test03() throws Throwable {
+	Appt appt3 = new Appt(0, 30, 1, 2, 2000, "Nothing", "Empty Day", "person@gmail.com");
+	int [] num = null;
+	appt3.setRecurrence(num, 1, 1, 1);
+	assertTrue(appt3.isRecurring());
+	assertTrue(appt3.getValid());
+	appt3.setValid();
+}
+
+@Test(timeout = 4000)
+	public void test04() throws Throwable {
+	Appt appt4 = new Appt(70,1,2, 3, -1, null, null, null);
+	appt4.setValid();
+}
+
+@Test(timeout = 4000)
+	public void test05() throws Throwable {
+	Appt appt5 = new Appt(3, -1, 1, 2, 2018, null, null, null);
+	appt5.setValid();
+}
+
+@Test(timeout = 4000) 
+	public void test06() throws Throwable {
+	Appt appt6 = new Appt(3, 3, 1, 2, -3, null, null, null);
+	appt6.setValid();
+}
+
+@Test(timeout = 4000) 
+	public void test07() throws Throwable {
+	Appt appt7 = new Appt(3, 3, 0, 2, 2018, null, null, null);
+	appt7.setValid();
+}
+@Test(timeout = 4000) 
+	public void test08() throws Throwable {
+	Appt appt8 = new Appt(2, 2, 2, 2, 2004, "Nothing", "Empty Day", "person@gmail.com");
+	appt8.setValid();
+}
+@Test(timeout = 4000) 
+	public void test09() throws Throwable {
+	Appt appt9 = new Appt(0, 30, 1, 2, 1000, "Nothing", "Empty Day", "person@gmail.com");
+	appt9.setValid();
+}
+
+@Test(timeout = 4000) 
+	public void test10() throws Throwable {
+	Appt appt9 = new Appt(-1, -1, -1, -1, -1, "Nothing", "Empty Day", "person@gmail.com");
+	appt9.setValid();
+	assertFalse(appt9.getValid());
+}
+
 
 }
--- projects/guptaso/Assignment-3/Calendar/src/test/java/calendar/DataHandlerTest.java
+++ projects/guptaso/Assignment-3/Calendar/src/test/java/calendar/DataHandlerTest.java
@@ -15,14 +15,111 @@ import java.util.LinkedList;
 
 
 public class DataHandlerTest{
+  Appt appt0 = new Appt(1, 30, 20, 4, 2018, "Homework", "Study Day", "person@gmail.com");
+  Appt appt1 = new Appt(1, 30, 20, 4, 2018, "Homework", "Study Day", "person@gmail.com");
+  Appt invalidAppt = new Appt(-1, -1, -1, -1, -1, "Nothing", "Empty Day", "person@gmail.com");
+  GregorianCalendar apt5 = new GregorianCalendar(2018, 4, 20, 11, 49, 00);
+  GregorianCalendar apt6 = new GregorianCalendar(2018, 2, 20, 11, 49, 00);
+
 
   @Test(timeout = 4000)
   public void test00()  throws Throwable  {
-
+	DataHandler dh0 = new DataHandler();
+	assertTrue(dh0.saveAppt(appt0));
+	assertTrue(dh0.deleteAppt(appt0));
   }
+
   @Test(timeout = 4000)
   public void test01()  throws Throwable  {
-
+	DataHandler dh1 = new DataHandler("fileName", false);
+	assertTrue(dh1.save());
+	assertTrue(dh1.saveAppt(appt0));
+	assertTrue(dh1.deleteAppt(appt0));
   }
 
+  @Test(timeout = 4000) 
+  public void test02() throws Throwable {
+	DataHandler dh2 = new DataHandler("filename0");
+        GregorianCalendar apt1 = new GregorianCalendar(2018, 4, 20, 11, 49, 00);
+        GregorianCalendar apt2 = new GregorianCalendar(2018, 2, 20, 11, 49, 00);
+	dh2.getApptRange(apt2, apt1);
+  }
+	
+  @Test(timeout = 4000) 
+  public void test03() throws Throwable {
+	DataHandler dh3 = new DataHandler("filename1");
+  	int [] num = {1,2};
+	appt1.setRecurrence(num, 1, 1, 1);
+	assertTrue(dh3.saveAppt(appt0));
+	assertTrue(dh3.saveAppt(appt1));
+  }
+
+  @Test(timeout = 4000) 
+  public void test04() throws Throwable {
+	DataHandler dh4 = new DataHandler("filename2");
+	assertTrue(dh4.saveAppt(appt0));
+	assertTrue(dh4.saveAppt(appt1));
+	dh4.getApptRange(apt6, apt5);
+  }
+
+  @Test(timeout = 4000) 
+  public void test05() throws Throwable {
+	DataHandler dh5 = new DataHandler("filename3");
+	int[] numArr = {};
+	appt0.setRecurrence(numArr, appt0.RECUR_BY_WEEKLY, 2, 1);
+	assertTrue(dh5.saveAppt(appt0));
+	assertTrue(dh5.saveAppt(appt1));
+	dh5.getApptRange(apt6, apt5);
+  
+}
+  
+  @Test(timeout = 4000)
+  public void test06() throws Throwable {
+	DataHandler dh6 = new DataHandler("filename4");
+	int[] numArr = {1};
+	appt0.setRecurrence(numArr, appt0.RECUR_BY_YEARLY, 2, 2);
+	assertTrue(dh6.saveAppt(appt0));
+	dh6.getApptRange(apt6, apt5);
+  }
+
+  @Test(timeout = 4000) 
+  public void test07() throws Throwable {
+	DataHandler dh7 = new DataHandler("filename5");
+	invalidAppt.setValid();
+	assertFalse(dh7.saveAppt(invalidAppt));
+	assertFalse(dh7.deleteAppt(invalidAppt));
+  }
+
+  @Test(timeout = 4000)
+  public void test08() throws Throwable {
+	DataHandler dh8 = new DataHandler("filename6");
+	int[] numArr = {90};
+	appt0.setRecurrence(numArr, 9, 9, 9);
+	assertTrue(dh8.saveAppt(appt0));
+	dh8.getApptRange(apt6, apt5);
+  }
+
+  @Test(timeout = 4000)
+  public void test09() throws Throwable {
+	DataHandler dh9 = new DataHandler("filename7");
+	appt0.setXmlElement(null);
+	dh9.deleteAppt(appt0);
+	int[] numArr = {1,2,3,2,3};
+	appt0.setRecurrence(numArr, appt0.RECUR_BY_WEEKLY, 3, 3);
+	assertTrue(dh9.saveAppt(appt0));
+	dh9.getApptRange(apt6, apt5);
+  }
+
+  @Test(timeout = 4000)
+  public void test10() throws Throwable {
+	DataHandler dh10 = new DataHandler("filename8");
+	Appt appt90 = new Appt(1, 30, 20, 4, 2018, "Homework", "Study Day", "person@gmail.com");
+	assertTrue(dh10.saveAppt(appt90));
+	try {
+		dh10.getApptRange(apt5, apt6);
+	}
+	catch(Exception e){
+	}
+   
+  }
 }
