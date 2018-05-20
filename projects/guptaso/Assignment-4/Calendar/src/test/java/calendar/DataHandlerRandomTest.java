628a43a2dbf2233e44b59b4ded39db26190186fb
Sonica Gupta <guptaso@flip1.engr.oregonstate.edu>
2018-05-19 22:56:51 -0700

    finish assign4

--- projects/guptaso/Assignment-4/Calendar/src/test/java/calendar/DataHandlerRandomTest.java
+++ projects/guptaso/Assignment-4/Calendar/src/test/java/calendar/DataHandlerRandomTest.java
@@ -23,10 +23,9 @@ public class DataHandlerRandomTest {
      */
 	 @Test
 	  public void radnomtest()  throws Throwable  {
-		DataHandler dh = new DataHandler("fileName", false);
-		DataHandler dh1 = new DataHandler("fileName", true);
-//		DataHandler dh2 = new DataHandler("fileName");
-		DataHandler dh3 = new DataHandler();
+
+		DataHandler dh = new DataHandler("fileName");
+		DataHandler dh1 = new DataHandler("fileName2");
 		long randomseed = System.currentTimeMillis();
 		Random random = new Random(randomseed);
 
@@ -37,15 +36,13 @@ public class DataHandlerRandomTest {
 			int hour = ValuesGenerator.getRandomIntBetween(random, 0,14);
 			int min = ValuesGenerator.getRandomIntBetween(random, 0,90);
 			int sec = ValuesGenerator.getRandomIntBetween(random, 0,90);
+		
 			Appt appt1 = new Appt(hour, min, day, month, year, "HW", "HW Day", "p@gmai.com");
+
+			int[] numArr = {1,2,3,2,3};
+			appt1.setRecurrence(numArr, appt1.RECUR_BY_WEEKLY, 3, 3);
+
 			appt1.setValid();
-			dh.saveAppt(appt1);
-
-			dh3.saveAppt(appt1);
-			dh3.deleteAppt(appt1);	
-
-//			int[] numArr = {1,2,3,2,3};
-//			appt1.setRecurrence(numArr, appt1.RECUR_BY_WEEKLY, 3, 3);
 
 			int year2 = ValuesGenerator.getRandomIntBetween(random,2018,2018);
 			int day2 = ValuesGenerator.getRandomIntBetween(random, 0,30);
@@ -55,8 +52,13 @@ public class DataHandlerRandomTest {
 			int sec2 = ValuesGenerator.getRandomIntBetween(random, 0,90);
 			Appt appt2 = new Appt(hour2, min2, day2, month2, year2, "HW", "HW Day", "p@gmai.com");
 			appt2.setValid();
+
+			dh.saveAppt(appt1);
+			dh.saveAppt(appt2);	
+		
+			dh1.saveAppt(appt1);
 			dh1.saveAppt(appt2);
-			dh1.saveAppt(appt1);	
+
 			GregorianCalendar apt1 = new GregorianCalendar(year, month, day, hour, min, sec);
 			GregorianCalendar apt2 = new GregorianCalendar(year2, month2, day2, hour2, min2, sec2);
 			try {
@@ -64,15 +66,17 @@ public class DataHandlerRandomTest {
 			}
 			catch(Exception e) {
 			}
-
 			try {
 				dh1.getApptRange(apt1, apt2);
 			}
 			catch(Exception e) {
 			}
-			dh.deleteAppt(appt1);
-			dh1.deleteAppt(appt2);
-	 	}
 
+			dh.deleteAppt(appt1);
+			dh.deleteAppt(appt2);
+			dh1.deleteAppt(appt1);
+			dh1.deleteAppt(appt2);
+
+	 	}
 	}
 }
