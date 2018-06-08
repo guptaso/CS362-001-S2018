f97cf12bddd372f1115f8beb760af4c04966f0f7
Lauren Sunamoto <sunamotl@flip1.engr.oregonstate.edu>
2018-06-08 15:36:09 -0700

    added stuff

--- projects/guptaso/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/guptaso/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -23,7 +23,38 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 //You can use this function to implement your manual testing	   
-	   
+	url = "";
+	System.out.println(urlValidatorInCorrect.isValid(url));
+	
+	url = ":// www.google.com:80/test1?action=view";
+	System.out.println(urlValidatorInCorrect.isValid(url));
+
+	url = " http://www.google.com:80/test1?action=view";
+	System.out.println(urlValidatorInCorrect.isValid(url));
+
+	url = "www.cs.com";
+	System.out.println(urlValidatorInCorrect.isValid(url));
+
+	url = " https://www.google.com:80/test1?action=view";
+	System.out.println(urlValidatorInCorrect.isValid(url));
+
+	url = "http://www.yahoo.com";	
+	System.out.println(urlValidatorInCorrect.isValid(url));
+   
+	System.out.println("EXPECTED\n:");
+
+   	//Expect: invalid Result: 
+   	System.out.println(urlValidatorInCorrect.isValid(""));
+	//Expect: invalid Result: 
+	System.out.println(urlValidatorInCorrect.isValid(":// www.google.com:80/test1?action=view"));
+   	//Expect: valid Result: 
+	System.out.println(urlValidatorInCorrect.isValid(" http://www.google.com:80/test1?action=view"));
+   	//Expect: invalid Result: 
+	System.out.println(urlValidatorInCorrect.isValid("www.cs.com"));
+   	//Expect: valid Result: 
+	System.out.println(urlValidatorInCorrect.isValid(" https://www.google.com:80/test1?action=view"));
+   	//Expect: valid Result: 
+	System.out.println(urlValidatorInCorrect.isValid("http://www.yahoo.com"));
    }
    
    
